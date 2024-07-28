9847969767594d28c4f84b40a1155ceb33f0b294
Roman <Juhimenkoroman@mail.ru>
2024-07-28 16:38:03 +0500

    add

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World! V1';
+  let message = req.query.message || req.body.message || 'Hello World! V2';
   res.status(200).send(message);
 };
