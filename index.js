ada25e50f91640645839e6984e3e378f395f8fea
Stromale-ISO <juhimenkoroman@gmail.com>
2024-07-28 20:51:50 +0500

    Update index.js
    l

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World! V12';
+  let message = req.query.message || req.body.message || 'Hello World! V13';
   res.status(200).send(message);
 };
